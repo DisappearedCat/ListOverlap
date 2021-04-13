82b24ced382f64fe268980c054ac4e4c52738a94
Nail Minnigalin <nail160306@gmail.com>
2021-04-13 21:15:41 +0300

    Add list_overlap

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def list_overlap(list1, list2):
-    pass
+    ret_list = []
+    for i in list1:
+        if (i in list2) and not (i in ret_list):
+            ret_list.append(i)
+
+    return ret_list
 
 if __name__ == '__main__':
     pass
