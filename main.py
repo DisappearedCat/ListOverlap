9fe0fb8db70076d1052e736e641cebba205f21dd
Nail Minnigalin <nail160306@gmail.com>
2021-04-13 21:20:53 +0300

    Reformat

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def list_overlap(list1, list2):
 
     return ret_list
 
+
 if __name__ == '__main__':
     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
     b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-    print(list_overlap(a , b))
+    print(list_overlap(a, b))
